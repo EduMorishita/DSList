5c2bbc36105643952ace7d089a8cf3c8f5f2bea6
eduardo.morishita6@gmail.com <eduardo.morishita6@gmail.com>
2023-07-28 22:14:00 -0300

    project created

---
+++ src/main/java/com/devsuperior/dslist/DevSuperiorApplication.java
@@ -0,0 +1,15 @@
+package com.devsuperior.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSuperiorApplication.class, args);
+		
+		
+	}
+
+}
